91ff18c3564362b6afa05b110dee407801b66b5f
Yingyu Cheng <github@winguse.com>
2022-10-15 20:19:13 -0700

    Initial Commit

---
+++ Locus/LocusApp.swift
@@ -0,0 +1,20 @@
+//
+//  LocusApp.swift
+//  Locus
+//
+//  Created by Yingyu Cheng on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LocusApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
